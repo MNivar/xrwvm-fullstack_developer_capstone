1bcf94b0fae0bcecaddca4860708fa09fc546cc7
“Madeline” <Madeline.nivar@gmail.com>
2024-07-30 12:21:44 -0400

    created CarModel and CarMake models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,23 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarModel ,CarMake
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 5
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['name', 'description']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['name', 'type', 'year', 'car_make']
+    inlines = [CarModelInline]
 
 # Register models here
+admin.site.register(CarModel)
+admin.site.register(CarMake)
