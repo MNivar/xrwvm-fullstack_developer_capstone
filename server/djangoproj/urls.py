eece2a431ac13ef64675c3d5bca4e8177ae27bd1
MNivar <104579355+MNivar@users.noreply.github.com>
2024-08-01 15:42:53 -0400

    Update urls.py
    clean up

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -14,26 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-
-
-
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('djangoapp/', include('djangoapp.urls')),
-#     re_path(r'^manifest.json$', serve, {'document_root': settings.STATIC_ROOT, 'path': 'manifest.json'}),
-#     path('', TemplateView.as_view(template_name="index.html")),
-#     path('about/', TemplateView.as_view(template_name="index.html")),
-#     path('contact/', TemplateView.as_view(template_name="index.html")),
-#     path('login/', TemplateView.as_view(template_name="index.html")),
-#     path('register/', TemplateView.as_view(template_name="index.html")),
-#     path('dealers/', TemplateView.as_view(template_name="index.html")),
-#     path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
-# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -44,6 +28,8 @@ urlpatterns = [
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>', TemplateView.as_view(template_name=
+                                                        "index.html")),
+    path('postreview/<int:dealer_id>', TemplateView.as_view(template_name=
+                                                           "index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
