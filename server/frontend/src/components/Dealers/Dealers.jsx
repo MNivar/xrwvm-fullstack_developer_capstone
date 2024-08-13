99c05968031415290f4d68bf33d5537fc79e73ab
“Madeline” <Madeline.nivar@gmail.com>
2024-08-12 22:09:49 -0400

    states dropdown changed nto a searchable textbox

--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -6,8 +6,11 @@ import review_icon from "../assets/reviewicon.png"
 
 const Dealers = () => {
   const [dealersList, setDealersList] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [originalDealers, setOriginalDealers] = useState([]);
   // let [state, setState] = useState("")
   let [states, setStates] = useState([])
+
 
   // let root_url = window.location.origin
   let dealer_url ="/djangoapp/get_dealers";
@@ -40,11 +43,30 @@ const Dealers = () => {
 
       setStates(Array.from(new Set(states)))
       setDealersList(all_dealers)
+      setOriginalDealers(all_dealers);
     }
   }
+
+     const handleInputChange = (event) => {
+        const query = event.target.value;
+        setSearchQuery(query);
+        const filtered = originalDealers.filter(dealer =>
+        dealer.state.toLowerCase().includes(query.toLowerCase())
+        )   ;
+        setDealersList(filtered);
+    };
+
+    const handleLostFocus = () => {
+        if (!searchQuery) {
+        setDealersList(originalDealers);
+        }
+    }
+
   useEffect(() => {
     get_dealers();
   },[]);  
+
+  
 
 
 let isLoggedIn = sessionStorage.getItem("username") != null ? true : false;
@@ -60,13 +82,19 @@ return(
       <th>Address</th>
       <th>Zip</th>
       <th>
-      <select name="state" id="state" onChange={(e) => filterDealers(e.target.value)}>
+      {/* <select name="state" id="state" onChange={(e) => filterDealers(e.target.value)}>
       <option value="" selected disabled hidden>State</option>
       <option value="All">All States</option>
       {states.map(state => (
           <option value={state}>{state}</option>
       ))}
-      </select>        
+      </select>         */}
+      <input type="text" placeholder="Search states..." onChange={handleInputChange} onBlur={handleLostFocus} value={searchQuery} />
+      {/* <option value="" selected disabled hidden>State</option>
+      <option value="All">All States</option>
+      {states.map(state => (
+          <option value={state}>{state}</option>
+      ))} */}
 
       </th>
       {isLoggedIn ? (
